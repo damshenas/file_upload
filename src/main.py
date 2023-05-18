8b3da81016dcf296a7427cbb117c84549d1cd1a9
damshenas <you@example.com>
2023-05-18 22:58:31 +0200

    some improvements

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
+# this function
+# gets the simple html page
+# updates the login page and logout page address
+# returns the content
+
 import json
 import boto3
 import logging
@@ -9,20 +14,25 @@ from botocore.exceptions import ClientError
 images_bucket = os.environ['UPLOADER_IMAGES_BUCKET']
 default_signedurl_expiry_seconds = os.environ['DEFAULT_SIGNEDURL_EXPIRY_SECONDS']
 
-# this function
-# creates a pre-sighned URL for uploading image to S3 and returns it
-
 def handler(event, context):
+
     uniquehash = hashlib.sha1("{}".format(time.time_ns()).encode('utf-8')).hexdigest()
-    result = create_presigned_post(images_bucket, "new/{}/{}".format(uniquehash[:2],uniquehash))
+    result = create_presigned_post(images_bucket, "new/{}|".format(uniquehash[:5]) + "${filename}")
 
     return {
         'statusCode': 200,
         'headers': {
-            'Content-Type': 'application/json; charset=UTF-8'
+            'Content-Type': 'text/html'
         },
-        'body': json.dumps(result)
+        'body': file_get_contents("index.html").replace('###preSignedUrl###', json.dumps(result))
     }
+
+def file_get_contents(filename):
+    with open(filename) as f:
+        return f.read()
+
+# this function
+# creates a pre-sighned URL for uploading image to S3 and returns it
 
 def create_presigned_post(bucket_name, object_name, fields=None, conditions=None, expiration=default_signedurl_expiry_seconds):
     s3_client = boto3.client('s3')
--- stack/cdk.py
+++ stack/cdk.py
@@ -1,9 +1,5 @@
-import yaml
-import typing
-
 from aws_cdk import (
     aws_s3 as _s3,
-    aws_cloudformation,
     Stack,
     CfnOutput
 )
@@ -25,13 +21,17 @@ from aws_cdk.aws_lambda import (
 
 from constructs import Construct
 
+configs = {
+    "ProjectName" : "S3FILEUPLOAD",
+    "Functions": {
+        "DefaultSignedUrlExpirySeconds": "86400"
+        }
+}
+
 class S3ImageUploaderStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-
-        with open("stack/config.yml", 'r') as stream:
-            configs = yaml.safe_load(stream)
 
         ### S3 core
         images_S3_bucket = _s3.Bucket(self, "S3_IMAGES")
@@ -45,15 +45,17 @@ class S3ImageUploaderStack(Stack):
         api_gateway = RestApi(self, 'UPLOADER_API_GATEWAY', rest_api_name='S3ImageUploaderApiGateway')
         api_gateway_resource = api_gateway.root.add_resource(configs["ProjectName"])
         api_gateway_landing_page_resource = api_gateway_resource.add_resource('web')
-        api_gateway_get_signedurl_resource = api_gateway_resource.add_resource('signedUrl')
-        api_gateway_image_search_resource = api_gateway_resource.add_resource('search')
 
         ### landing page function
         get_landing_page_function = Function(self, "UPLOADER_GET_LANDING_PAGE",
             function_name="UPLOADER_GET_LANDING_PAGE",
+            environment={
+                "UPLOADER_IMAGES_BUCKET": images_S3_bucket.bucket_name,
+                "DEFAULT_SIGNEDURL_EXPIRY_SECONDS": configs["Functions"]["DefaultSignedUrlExpirySeconds"]
+            },
             runtime=Runtime.PYTHON_3_7,
             handler="main.handler",
-            code=Code.from_asset("./src/landingPage"))
+            code=Code.from_asset("./src"))
 
         get_landing_page_integration = LambdaIntegration(
             get_landing_page_function,
@@ -72,49 +74,15 @@ class S3ImageUploaderStack(Stack):
                     'method.response.header.Access-Control-Allow-Origin': True
                 }
             )])
-
-        ### get signed URL function
-        get_signedurl_function = Function(self, "UPLOADER_GET_SIGNED_URL",
-            function_name="UPLOADER_GET_SIGNED_URL",
-            environment={
-                "UPLOADER_IMAGES_BUCKET": images_S3_bucket.bucket_name,
-                "DEFAULT_SIGNEDURL_EXPIRY_SECONDS": configs["Functions"]["DefaultSignedUrlExpirySeconds"]
-            },
-            runtime=Runtime.PYTHON_3_7,
-            handler="main.handler",
-            code=Code.from_asset("./src/getSignedUrl"))
-
-        get_signedurl_integration = LambdaIntegration(
-            get_signedurl_function,
-            proxy=True,
-            integration_responses=[IntegrationResponse(
-                status_code='200',
-                response_parameters={
-                   'method.response.header.Access-Control-Allow-Origin': "'*'",
-                }
-            )])
-
-        get_signedurl_method = api_gateway_get_signedurl_resource.add_method('GET', get_signedurl_integration,
-            method_responses=[MethodResponse(
-                status_code='200',
-                response_parameters={
-                    'method.response.header.Access-Control-Allow-Origin': True,
-                }
-            )])
         
-        signedurl_custom_resource = typing.cast("aws_cloudformation.CfnCustomResource", get_signedurl_method.node.find_child('Resource'))
-        signedurl_custom_resource.add_property_override('AuthorizerId', api_gateway_get_signedurl_authorizer.ref)
-
-        images_S3_bucket.grant_put(get_signedurl_function, objects_key_pattern="new/*")
+        images_S3_bucket.grant_put(get_landing_page_function, objects_key_pattern="new/*")
 
         ### API gateway finalizing
-        self.add_cors_options(api_gateway_get_signedurl_resource)
         self.add_cors_options(api_gateway_landing_page_resource)
-        self.add_cors_options(api_gateway_image_search_resource)
 
         ### outputs
         CfnOutput(self, 'LandingPage',
-            value=api_gateway.url_for_path('/web'),
+            value="{}{}/web".format(api_gateway.url_for_path('/'), configs['ProjectName']),
             description='The landing page'
         )
 
